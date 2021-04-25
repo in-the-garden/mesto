32cea376da4b4b00889a05c6eab3c10f67e74d84
Irina Pupkova <pupkova.ia@yandex.ru>
2021-04-25 11:28:33 +0500

    feat: finish pop-up

--- spripts/index.js
+++ spripts/index.js
@@ -16,15 +16,17 @@ function togglePopup(event) {
 openPopupButton.addEventListener('click', togglePopup);
 closePopupButton.addEventListener('click', togglePopup);
 
+
 let formElement = document.querySelector('.popup__form')
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();    
+function formSubmitHandler (event) {
+    event.preventDefault();    
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popup.classList.toggle('popup_is-opened');
 }
 
+
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('sumbit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler); 
