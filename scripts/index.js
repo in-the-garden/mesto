f56181a22e11db2c0eb921cc91fffdfd164f51bf
Irina Pupkova <pupkova.ia@yandex.ru>
2021-05-06 21:09:07 +0500

    feature: add remove button

--- scripts/index.js
+++ scripts/index.js
@@ -17,9 +17,11 @@ let nameInput = popupEdit.querySelector('.popup__input_info_name');
 let jobInput = popupEdit.querySelector('.popup__input_info_job');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle'); 
+let formProfile = popupEdit.querySelector('.popup__form_submit_profile')
 
 let titleInput = popupAdd.querySelector('.popup__input_place_title');
 let linkInput = popupAdd.querySelector('.popup__input_place_link');
+let formPlaces = popupAdd.querySelector('.popup__form_submit_place')
 
 function openPopup(popupForm) {
     if (popupForm === popupEdit) {
@@ -35,14 +37,20 @@ function closePopup(popupForm) {
     popupForm.classList.remove('popup_is-opened');
 }
 
-let formProfile = popupEdit.querySelector('.popup__form_submit_profile')
-
 function formSubmitHandler (event) {
     event.preventDefault();    
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popupEdit.classList.remove('popup_is-opened');
 }
+
+function formAddNewCard (evt) {
+    evt.preventDefault();    
+    const cardName = titleInput.value;
+    const cardLink = linkInput.value;
+    cardsContainer.append(createNewCard(cardName, cardLink));
+    popupAdd.classList.remove('popup_is-opened');
+};
 
 openEditFrom.addEventListener('click', function() {
     openPopup(popupEdit);
@@ -58,18 +66,9 @@ openAddForm.addEventListener('click', function() {
 closeAddForm.addEventListener('click', function() {
     closePopup(popupAdd);
 });
- let formPlaces = popupAdd.querySelector('.popup__form_submit_place')
- 
- function formAddNewCard (event) {
-    event.preventDefault();    
-    const cardName = titleInput.value;
-    const cardLink = linkInput.value;
-    cardsContainer.append(createNewCard(cardName, cardLink));
-    popupAdd.classList.remove('popup_is-opened');
-};
-
 
 formPlaces.addEventListener('submit', formAddNewCard); 
+
 ///// Template-часть
 
 const cardsContainer = document.querySelector('.elements');
@@ -103,10 +102,22 @@ const initialCards = [
   ];
 
 function createNewCard(cardName, cardLink) {
+    function handleRemoveCard(evt) {
+        evt.target.closest('.element').remove();
+    };
+
     const newCard = cardTemplate.content.querySelector('.element').cloneNode('true');
     newCard.querySelector('.element__title').textContent = cardName;
     newCard.querySelector('.element__image').src = cardLink;
     newCard.querySelector('.element__image').alt = cardName;
+
+    newCard.querySelector('.element__heart-button').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('element__heart-button_active');
+    });
+
+    const cardRemoveButton = newCard.querySelector('.element__remove-button');
+    cardRemoveButton.addEventListener('click', handleRemoveCard);
+
     return newCard;
 }
 
