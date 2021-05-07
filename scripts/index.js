c99078a60bec5122fd1b6b6b64f7d0b433cc3ed1
Irina Pupkova <pupkova.ia@yandex.ru>
2021-05-08 00:59:14 +0500

    fix: adjust the layout

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,8 @@ const popupAdd = document.querySelector('.popup__place');
 const openAddForm = document.querySelector('.profile__add-button');
 const closeAddForm = document.querySelector('.popup__close_btn_place');
 
+const popupImage = document.querySelector('.popup__images');
+const closeImage = document.querySelector('.popup__close_btn_image');
 
 let nameInput = popupEdit.querySelector('.popup__input_info_name');
 let jobInput = popupEdit.querySelector('.popup__input_info_job');
@@ -24,21 +26,50 @@ let titleInput = popupAdd.querySelector('.popup__input_place_title');
 let linkInput = popupAdd.querySelector('.popup__input_place_link');
 let formPlaces = popupAdd.querySelector('.popup__form_submit_place');
 
+let fullImage = popupImage.querySelector('.popup__image');
+let imageTitle = popupImage.querySelector('.popup__title-image');
 
+const cardsContainer = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card-template');
+const imageTemplate = cardTemplate.querySelector('.element__image');
+const titleTemplate = cardTemplate.querySelector('.element__title');
 
+const initialCards = [
+    {
+      name: 'Гаваи',
+      link: './images/hawaii.jpg'
+    },
+    {
+      name: 'Вулкан Мауна-Лоа',
+      link: './images/mauna-loa.jpg'
+    },
+    {
+      name: 'Норвегия',
+      link: './images/norway.jpg'
+    },
+    {
+      name: 'Большой каньон',
+      link: './images/grand-canyon.jpg'
+    },
+    {
+      name: 'Исландия',
+      link: './images/iceland.jpg'
+    },
+    {
+      name: 'Фареры',
+      link: './images/faroe-islands.jpg'
+    }
+  ];
 
 
 function openPopup(popupForm) {
+    popupForm.classList.add('popup_is-opened');
     if (popupForm === popupEdit) {
-        popupForm.classList.add('popup_is-opened');
         nameInput.value = profileName.textContent;
         jobInput.value = profileJob.textContent; 
-    } else if (popupForm === popupAdd) {
-        popupForm.classList.add('popup_is-opened');
+    } else {
         titleInput.value = "";
         linkInput.value = "";
-    } else {
-        popupForm.classList.add('popup_is-opened');
     }
 }
 
@@ -77,49 +108,6 @@ closeAddForm.addEventListener('click', function() {
 });
 
 formPlaces.addEventListener('submit', formAddNewCard); 
-
-
-
-///// Template-часть
-
-const cardsContainer = document.querySelector('.elements');
-const cardTemplate = document.querySelector('#card-template');
-const imageTemplate = cardTemplate.querySelector('.element__image');
-const titleTemplate = cardTemplate.querySelector('.element__title');
-
-const initialCards = [
-    {
-      name: 'Гаваи',
-      link: './images/hawaii.jpg'
-    },
-    {
-      name: 'Вулкан Мауна-Лоа',
-      link: './images/mauna-loa.jpg'
-    },
-    {
-      name: 'Норвегия',
-      link: './images/norway.jpg'
-    },
-    {
-      name: 'Большой каньон',
-      link: './images/grand-canyon.jpg'
-    },
-    {
-      name: 'Исландия',
-      link: './images/iceland.jpg'
-    },
-    {
-      name: 'Фареры',
-      link: './images/faroe-islands.jpg'
-    }
-  ];
-
-  const popupImage = document.querySelector('.popup__images');
-  const closeImage = document.querySelector('.popup__close_btn_image');
-
-  let fullImage = popupImage.querySelector('.popup__image');
-  let imageTitle = popupImage.querySelector('.popup__title-image');
-  
  
 function openPopupImage(cardName, cardLink) {
     popupImage.classList.add('popup_is-opened');
@@ -127,7 +115,6 @@ function openPopupImage(cardName, cardLink) {
     fullImage.alt = cardName;
     imageTitle.textContent = cardName;
 }
-
 
 
 function createNewCard(cardName, cardLink) {
